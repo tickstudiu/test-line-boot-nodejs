2c8b6e116e515dd7a19ba0a313a599ca650033c3
tickstudiu <tickstudiu@gmail.com>
2019-05-29 18:11:06 +0700

    Initial project

---
+++ config.js
@@ -0,0 +1,3 @@
+export const channelAccessToken = "BNH/plK1S6GsGnhu+k81Nt9cUDRD5X9f+b9E/ydlc8Vr5n9T4whjBNsuXq299sIsJwM1wQ8N2LRuw/V+DiK1lJ2OUP8Gz7Nl3ook5WZALR4QNNLSpr3zN4O8Zf0W5xXCLjVLfU88LHPQSMTpQaSshwdB04t89/1O/w1cDnyilFU="
+export const channelSecret = "3618e986e135cb6dcfe0737fe13b1f1d"
+export const port = "3000"
